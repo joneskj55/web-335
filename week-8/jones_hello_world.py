75dbd581d52ba416d2d031634adfbc0d67b98039
joneskj55 <joneskj55@gmail.com>
2021-05-03 12:43:48 -0500

    add comments

--- week-8/jones_hello_world.py
+++ week-8/jones_hello_world.py
@@ -1,1 +1,9 @@
+# ========================================
+# Title: Exercise 8.2 - Anaconda
+# Author: Kevin Jones
+# Date: 3 May 2021
+# Description: Hello World (Python)
+# ========================================
+
+# hello world
 print('Hello World')
