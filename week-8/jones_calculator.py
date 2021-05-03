12fcb941d826dc7b42ce3c1e3d8b2294212c63bd
joneskj55 <joneskj55@gmail.com>
2021-05-03 12:12:01 -0500

    add 8.3

---
+++ week-8/jones_calculator.py
@@ -0,0 +1,22 @@
+# ========================================
+# Title: Exercise 8.3 - Python in Action
+# Author: Kevin Jones
+# Date: 3 May 2021
+# Description: Calculator using Python
+# ========================================
+
+def add(add1, add2):
+    return add1 + add2
+
+
+def subtract(sub1, sub2):
+    return sub1 - sub2
+
+
+def divide(div1, div2):
+    return div1 / div2
+
+
+print(add(1, 2))
+print(subtract(4, 1))
+print(divide(8, 2))
