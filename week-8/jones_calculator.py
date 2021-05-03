f6bfe2dc7533a17bc41fbf9e40ecf83b85bcb6cf
joneskj55 <joneskj55@gmail.com>
2021-05-03 12:16:28 -0500

    add comments

--- week-8/jones_calculator.py
+++ week-8/jones_calculator.py
@@ -5,18 +5,19 @@
 # Description: Calculator using Python
 # ========================================
 
+# add function
 def add(add1, add2):
     return add1 + add2
 
-
+# subtract function
 def subtract(sub1, sub2):
     return sub1 - sub2
 
-
+# divide function
 def divide(div1, div2):
     return div1 / div2
 
-
+# output answers
 print(add(1, 2))
 print(subtract(4, 1))
 print(divide(8, 2))
